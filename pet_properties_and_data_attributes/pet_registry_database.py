3f43f11fae45e4f09eb19ebcfed28dcf1de26b2c
ylla-b-28 <ylla.briza28@gmail.com>
2026-06-05 02:57:00 +0800

    I added the color designs.

--- pet_properties_and_data_attributes/pet_registry_database.py
+++ pet_properties_and_data_attributes/pet_registry_database.py
@@ -22,3 +22,18 @@ class Pet:
     def get_age(self):
         return self.__age
 
+class TestPet:
+    def display_pet_properties(self, pet_object, pet_display_name):
+        print(f"\033[1;95m₊˚ ✧ ━━━━ ⊱\033[0m \033[1;38;5;129m{pet_display_name}\033[0m \033[1;95m⊰ ━━━━ ✧ ₊˚\033[0m")
+
+        name_value = pet_object.get_name()
+        type_value = pet_object.get_animal_type()
+        age_value = pet_object.get_age()
+
+        power_state_value = "\033[1;92m"
+        power_status_text_label = "Registered"
+
+        print(f"   \033[1;38;5;208mPet Name:\033[0m    \033[1;92m{name_value}\033[0m")
+        print(f"   \033[1;38;5;208mAnimal Type:\033[0m \033[1;93m{type_value}\033[0m")
+        print(f"   \033[1;38;5;208mPet Age:\033[0m     \033[1;38;5;129m{age_value}\033[0m")
+        print(f"   \033[1;38;5;208mStatus:\033[0m      {power_state_value}{power_status_text_label}\033[0m")
