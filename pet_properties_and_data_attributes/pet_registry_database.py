7e73319f62ff39218df70144262e2a18402e9102
ylla-b-28 <ylla.briza28@gmail.com>
2026-06-05 02:59:28 +0800

    I added instance and collect user input.

--- pet_properties_and_data_attributes/pet_registry_database.py
+++ pet_properties_and_data_attributes/pet_registry_database.py
@@ -37,3 +37,19 @@ class TestPet:
         print(f"   \033[1;38;5;208mAnimal Type:\033[0m \033[1;93m{type_value}\033[0m")
         print(f"   \033[1;38;5;208mPet Age:\033[0m     \033[1;38;5;129m{age_value}\033[0m")
         print(f"   \033[1;38;5;208mStatus:\033[0m      {power_state_value}{power_status_text_label}\033[0m")
+
+    def run_test(self):
+        my_pet = Pet()
+
+        print("")
+        user_name = input("\033[1;38;5;208mEnter your pet's name:\033[0m ")
+        user_type = input("\033[1;38;5;208mEnter your pet's animal type (e.g., Dog, Cat, Bird):\033[0m ")
+        user_age = input("\033[1;38;5;208mEnter your pet's age:\033[0m ")
+
+        my_pet.set_name(user_name)
+        my_pet.set_animal_type(user_type)
+        my_pet.set_age(user_age)
+
+        print("")
+        self.display_pet_properties(my_pet, "Pet Profile Object")
+        print("\033[1;95m⊱.˚── ⋅ .✧˚❀˚✧. ⋅ ──˚.⊰\033[0m")
