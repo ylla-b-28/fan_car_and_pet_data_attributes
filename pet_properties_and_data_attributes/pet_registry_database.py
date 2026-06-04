f892224750c60fdb28d96af67ebe8ba51b54f5ce
ylla-b-28 <ylla.briza28@gmail.com>
2026-06-05 02:51:12 +0800

    I added the getters and setters.

--- pet_properties_and_data_attributes/pet_registry_database.py
+++ pet_properties_and_data_attributes/pet_registry_database.py
@@ -4,3 +4,21 @@ class Pet:
         self.__animal_type = ""
         self.__age = 0
 
+    def set_name(self, name):
+        self.__name = str(name)
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = str(animal_type)
+
+    def set_age(self, age):
+        self.__age = int(age)
+
+    def get_name(self):
+        return self.__name
+
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        return self.__age
+
