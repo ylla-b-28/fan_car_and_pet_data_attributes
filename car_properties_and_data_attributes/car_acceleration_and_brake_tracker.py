0fd892ad4b2f1bcaa980734c3eabfcfea40e1ba6
ylla-b-28 <ylla.briza28@gmail.com>
2026-06-05 02:12:24 +0800

    I added getters.

--- car_properties_and_data_attributes/car_acceleration_and_brake_tracker.py
+++ car_properties_and_data_attributes/car_acceleration_and_brake_tracker.py
@@ -4,6 +4,31 @@ class Car:
         self.__make = str(make)
         self.__speed = 0
 
+    #method to add 5 to the speed attribute
+    def accelerate(self):
+        self.__speed += 5
+
+    #method to subtract 5 from the speed attribute
+    def brake(self):
+        if self.__speed >= 5:
+            self.__speed -= 5
+        else:
+            self.__speed = 0
+
+    #getter method for current speed
+    def get_speed(self):
+        return self.__speed
+
+    #getter method for year model
+    def get_year_model(self):
+        return self.__year_model
+
+    #getter method for make
+    def get_make(self):
+        return self.__make
+
+
+
 
 
 
