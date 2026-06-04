3f1d3973b7ee5a83fa028f3ad78592b8b42de6c3
ylla-b-28 <ylla.briza28@gmail.com>
2026-06-05 02:11:50 +0800

    I added variables.

--- car_properties_and_data_attributes/car_acceleration_and_brake_tracker.py
+++ car_properties_and_data_attributes/car_acceleration_and_brake_tracker.py
@@ -1,7 +1,11 @@
-#class
+class Car:
+    def __init__(self, year_model, make):
+        self.__year_model = str(year_model)
+        self.__make = str(make)
+        self.__speed = 0
 
-#variable
 
-#method
 
-#getter
+
+
+
