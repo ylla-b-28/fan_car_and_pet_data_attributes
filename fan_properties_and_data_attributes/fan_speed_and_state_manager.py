32c9b4a57cfc22d402d465c7710fd76e64c9d90d
ylla-b-28 <ylla.briza28@gmail.com>
2026-06-05 00:09:41 +0800

    I added getters and setters for all encapsulated fields.

--- fan_properties_and_data_attributes/fan_speed_and_state_manager.py
+++ fan_properties_and_data_attributes/fan_speed_and_state_manager.py
@@ -3,9 +3,43 @@ class Fan:
     medium = 2
     fast = 3
 
-def __init__(self, initial_speed=slow, initial_radius=5.0, initial_color="blue", initial_on_status=False):
-    self.__speed = initial_speed
-    self.__radius = float(initial_radius)
-    self.__color = str(initial_color)
-    self.__on_status = bool(initial_on_status)
+    def __init__(self, initial_speed=slow, initial_radius=5.0, initial_color="blue", initial_on_status=False):
+        self.__speed = initial_speed
+        self.__radius = float(initial_radius)
+        self.__color = str(initial_color)
+        self.__on_status = bool(initial_on_status)
+
+    #getter method for speed
+    def get_speed(self):
+        return self.__speed
+
+    #setter method for speed
+    def set_speed(self, new_speed):
+        self.__speed = new_speed
+
+    #getter method for radius
+    def get_radius(self):
+        return self.__radius
+
+    #setter method for radius
+    def set_radius(self, new_radius):
+        self.__radius = float(new_radius)
+
+    #getter method for color
+    def get_color(self):
+        return self.__color
+
+    #setter method for color
+    def set_color(self, new_color):
+        self.__color = str(new_color)
+
+    #getter method for power status
+    def get_on_status(self):
+        return self.__on_status
+
+    #setter method for power status
+    def set_on_status(self, new_on_status):
+        self.__on_status = bool(new_on_status)
+
+
 
